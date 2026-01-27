9c927437f55e704267dcf6f0e3d31e2a71990349
buildwithjaym <jaymmaruji@gmail.com>
2026-01-27 13:23:48 +0800

    sa

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -1,27 +1,40 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { studentRouter } from "./routes/student.routes.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.join(__dirname, "..", ".env") });
+console.log("ENV CHECK:", {
+  PORT: process.env.PORT,
+  SUPABASE_URL: process.env.SUPABASE_URL ? "OK" : "MISSING",
+  SUPABASE_SECRET_KEY: process.env.SUPABASE_SECRET_KEY ? "OK" : "MISSING",
+});
+
+
+
 import { authRouter } from "./routes/auth.routes.js";
-
-
-dotenv.config();
+import { studentRouter } from "./routes/student.routes.js";
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  credentials: true
+}));
 app.use(express.json());
-app.use("/student", studentRouter);
-app.use("/auth", authRouter);
 
 app.get("/health", (req, res) => {
-    res.json({ ok: true, service: "classify-api" });
+  res.json({ ok: true, service: "classify-api" });
 });
-app.use(cors({
-    origin: ["http://localhost:5173"],
-    credentials: true
-}));
+
+app.use("/auth", authRouter);
+app.use("/student", studentRouter);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-    console.log(`API running on http://localhost:${port}`);
+  console.log(`API running on http://localhost:${port}`);
 });
